169c2a766218f7397777ef9d03c0a1f996c569d8
Moubarak Congacou <cmouba1999@gmail.com>
2026-05-19 12:38:32 -0400

    Created the Person and Program Classes, added the methods as well as the conllection of arrayList

--- src/main/java/com/pluralsight/Person.java
+++ src/main/java/com/pluralsight/Person.java
@@ -1,4 +1,55 @@
 package com.pluralsight;
 
-public class Person {
+public class Person implements Comparable<Person> {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+
+    //Constructor
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    //Getters and setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    //An override method to compare objects
+    @Override
+    public int compareTo(Person other) {
+        return this.lastName.compareTo(other.lastName);
+    }
+
+
+    //An Override method (toString)
+    @Override
+    public String toString() {
+        return firstName + " " + lastName +  " - Age: " + age;
+    }
 }
--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,4 +1,31 @@
 package com.pluralsight;
+import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class Program {
+
+    public static void main(String[] args) {
+
+        //An ArrayList of family objects
+        ArrayList<Person> myFamily = new ArrayList<Person>();
+
+
+        //Adding objects(family members) to the arrayList
+        myFamily.add( new Person("Dana", "Wyatt", 63) );
+        myFamily.add( new Person("Zachary", "Westly", 31) );
+        myFamily.add( new Person("Elisha", "Aslan", 14) );
+        myFamily.add( new Person("Ian", "Auston", 16) );
+        myFamily.add( new Person("Zephaniah", "Hughes", 9) );
+        myFamily.add( new Person("Ezra", "Aiden", 17) );
+
+        Collections.sort(myFamily);
+
+        for (Person person : myFamily) {
+            System.out.println(person);
+        }
+
+    }
+
+
 }
